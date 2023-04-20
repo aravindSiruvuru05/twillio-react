63819804c6d5762ec261e8f657ab4533ff1ce88d
Aravind Siruvuru <aravind@thetransmogrify.in>
2023-04-20 09:51:25 +0530

    token changes

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -12,12 +12,9 @@ const ChatScreen = () => {
   const [chatClient, setChatClient] = useState(null)
   const [userIdentity, setUserIdentity] = useState('')
 
-  console.log(userIdentity, "useiude----", chatClient, "-- chatclietnt")
   useEffect(() => {
-    const userIdty = generateRandomName()
-    setUserIdentity(userIdty)
     setIsLoading(true)
-    getChatClient(userIdty)
+    getChatClient()
       .then((client) => {
         setChatClient(client)
       })
@@ -27,15 +24,19 @@ const ChatScreen = () => {
       .finally(() => setIsLoading(false))
   }, [])
 
-  const getChatClient = async (userIdty) => {
-    if (!userIdty) {
-      console.log('user Identity not provided to generate access token.')
-      return null
-    }
+  const getChatClient = async () => {
     try {
-      const res = await fetch(`http://localhost:3000/token/${userIdty}`)
-      const accessToken = await res.json()
-      const client = await TwilioClient.create(accessToken.token)
+      const res = await fetch(`http://localhost:3000/twilio/token`, {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InNMejZEdWlRcVF6YXAweFV1QkF0eiJ9.eyJpc3MiOiJodHRwczovL2Rldi0wdmYyZzJ1c29ubndpNDRyLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2M2U1NDAyNzU5NWM5YTk4NjdmMGM3NjkiLCJhdWQiOlsiaHR0cHM6Ly9yZWRnaXN0cnkuY29tIiwiaHR0cHM6Ly9kZXYtMHZmMmcydXNvbm53aTQ0ci51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjc5OTg1ODk3LCJleHAiOjE2ODAwNzIyOTcsImF6cCI6IkdBQTBHWFBtOWdzUHJJSXBMS0N0c0p6aUNJcUFpSGVZIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBhZGRyZXNzIHBob25lIG9mZmxpbmVfYWNjZXNzIiwiZ3R5IjoicGFzc3dvcmQifQ.T2GSk2OoBioJZNg4w3m2d8W5reaHbzBS0kki89RbKLlj-Je0eIVF2uyCUzxjZYBLQAThfZTeSoLQ1M9S1n4mm-GVvgEQJHCL_BXO4uVohvSBjtNHLMEg4Xri2A5dJJbP7Ahp478Sqs5X3UmI9aNLoLn57IBTTRUhcNYWo81UdI2TRNagAWjXS709-LPlgxL-tT8u1k4kadgJ3L7Y6PIzNDbXqTy0NJ9qZdP4DKtAVvVyjFNS505eBKEmvJoyAPgQpJRfsLq60sbFZWxpfMnbGVzbC7Z6L3sp7qHiK6JAdvuRQeBh8JbqCpKHGaAp2swzRi55ydXt9y9VvH4fl44oTg',
+        },
+      })
+      const response = await res.json()
+      const { data } = response
+      setUserIdentity(data.identity)
+
+      const client = await TwilioClient.create(data.token)
       return client
     } catch (e) {
       console.log(e)
@@ -48,15 +49,15 @@ const ChatScreen = () => {
   }
 
   if (isLoading) return <div style={{ marginTop: '50px' }}>isLoading...</div>
-  console.log(chatClient, "chat cliuent ----")
+  console.log(chatClient, 'chat cliuent ----')
   return (
     <div style={{ display: 'flex', alignItems: 'space-evenly' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
         <h5>Current User: {userIdentity}</h5>
         <CreateNewChat chatClient={chatClient} userIdentity={userIdentity} onCreateChat={handleChatChange} />
         <ChatsList chatClient={chatClient} currChatName={currChat?.uniqueName} userIdentity={userIdentity} onChatChange={handleChatChange} />
       </div>
-      <hr class="solid"></hr>
+      <hr class='solid'></hr>
       <ChatWindow currChat={currChat} />
     </div>
   )
